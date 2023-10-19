940db9f426469d4021344754ffa9f267daa294f0
MaelysCallens <maelys.callens@student.hogent.be>
2023-10-19 14:52:19 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Maëlys Callens on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
